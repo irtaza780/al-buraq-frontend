7c8e9b407e51555c73985ad75ca0be3c0df976c9
muhammadirtaza <muhammad.irtaza@codistan.org>
2023-03-25 11:57:14 +0500

    fix: package and env

--- src/services/api/axios.config.ts
+++ src/services/api/axios.config.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const dataServer = axios.create({
-  baseURL: "http://localhost:4000",
+  baseURL: process.env.REACT_APP_API_URL,
   timeout: 100000000,
   headers: { Accept: "application/json", "Content-Type": "application/json" },
 });
